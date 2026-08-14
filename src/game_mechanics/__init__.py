4f3f2dbc6f510c374e35b605072e887cdb4cd3fb
halldonovana <1599349738117002@mil>
2026-08-14 15:01:28 -0400

    set up init files

---
+++ src/game_mechanics/__init__.py
@@ -0,0 +1,12 @@
+# mechanics/__init__.py
+"""
+Core game mechanics including sparks, shadows, and progress clocks.
+"""
+
+from .mechanics import Sparks, Shadows, Clock
+
+__all__ = [
+    'Sparks',
+    'Shadows',
+    'Clock',
+]
